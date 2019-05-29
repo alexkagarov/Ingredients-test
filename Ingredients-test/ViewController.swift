fcd91b34973e5c37c69afc0a303466c00055ead5
Alex Kagarov <alexkagarov@mbp-dima.rusanovka-net.kiev.ua>
2019-05-29 22:12:58 +0300

    Initial Commit

---
+++ Ingredients-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ingredients-test
+//
+//  Created by Alex Kagarov on 5/29/19.
+//  Copyright © 2019 Alex Kagarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
